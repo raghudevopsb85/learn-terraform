8a1f8dfca2267007011b0b142251c10d5be8d47b
Raghu K <raghuk.vit@gmail.com>
2025-07-03 08:41:25 +0530

    RoboShop Code

--- 03-loops1/sample.tf
+++ 03-loops1/sample.tf
@@ -2,3 +2,15 @@ resource "null_resource" "dummy" {
   count = 10
 }
 
+provider "aws" {}
+
+resource "aws_instance" "web" {
+  count = 2
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "web"
+  }
+}
+
