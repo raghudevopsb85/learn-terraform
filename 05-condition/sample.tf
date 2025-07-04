2194c9b6326b68bffc2d75e19adf8b3ac54207cf
Raghu K <raghuk.vit@gmail.com>
2025-07-04 07:57:09 +0530

    RoboShop Code

--- 05-condition/sample.tf
+++ 05-condition/sample.tf
@@ -9,3 +9,15 @@ resource "aws_instance" "web" {
 
 variable "instance_type" {}
 
+# Conditions
+# Strings -  ==, !=
+# Numbers - == , !=, >, >=, <, <=
+# Booleans ( var.boolean ? true_val : false_val )
+
+
+# Condition can also be used in cases to determine whether to create a resource or not
+resource "null_resource" "test" {
+  count = var.input == "" ? 0 : 1
+}
+
+variable "input" {}
