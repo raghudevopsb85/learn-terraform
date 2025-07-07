3c7579a17f461f297c2d98c412442f2f14b27934
Raghu K <raghuk.vit@gmail.com>
2025-07-07 08:36:51 +0530

    RoboShop Code

--- 09-hashicorp-vault/test.tf
+++ 09-hashicorp-vault/test.tf
@@ -6,9 +6,9 @@ data "vault_generic_secret" "rundeck_auth" {
 # For this example, in Vault there is a key named "auth_token" and the value is the token we need to keep secret.
 # In general usage, replace "auth_token" with the key you wish to extract from Vault.
 
-provider "rundeck" {
-  url        = "http://34.236.156.171:8200"
-  auth_token = var.token
+provider "vault" {
+  address        = "http://34.236.156.171:8200"
+  token = var.token
 }
 
 variable "token" {}
